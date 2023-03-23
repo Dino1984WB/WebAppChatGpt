d3db26dbb22f4990db9f972d1708691fef0a4dad
Dino1984WB <williamkbukowskidev@gmail.com>
2023-03-23 11:39:44 -0400

    initial commit >     added 3 files ( js, html, and python backend)

---
+++ API.py
@@ -0,0 +1,12 @@
+#WIlliam Bukowski - Chatgpt3 used to produce most of the code 
+#you see here, this is simply to test the capabilities of chatgpt3 ( davinci model)
+#and my own abilitiy to utilize its code production
+
+
+# Create a list to store the items 
+list_of_items = []
+
+# Define a function to add items to the list 
+def add_item(item): 
+    list_of_items.append(item)
+
---
+++ HomePage.js
@@ -0,0 +1,6 @@
+//william bukowski: JS for front end of chatgpt3 list thingy
+
+// Create a function to add an item to the list 
+function addItem(item) {
+    list_of_items.push(item);
+  }
